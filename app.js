3a6b86c31a862cc315717f97addc92bedfc1691a
JohnnyFreire97 <johnnyfreirejh@hotmail.com>
2021-09-21 20:59:42 -0300

    Add new package

---
+++ app.js
@@ -0,0 +1,9 @@
+let readlineSync = require('readline-sync');
+
+var data_atual, data_evento, idade;
+var list_usuario;
+
+data_atual = readlineSync.questionInt("Digite a data atual: Somente o dia! ");
+data_evento = readlineSync.questionInt("Digite a data do Evento: Somente o dia! ");
+
+
